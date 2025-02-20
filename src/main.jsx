c8861da7f5870a9e5e8acdb39eb0069e650efb12
Sumin-Lee12 <leesumin041@gmail.com>
2025-02-20 20:45:06 +0900

    fix: QueryClient 오류 해결

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
+import App from "./App.jsx";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRoot } from "react-dom/client";
 
-import App from './App.jsx'
-import React from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={QueryClient}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
