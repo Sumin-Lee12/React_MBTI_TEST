24222c701c9afefda1cece894195b582bd818794
Sumin-Lee12 <leesumin041@gmail.com>
2025-02-24 16:56:55 +0900

    feat: MBTI 테스트를 할 수 있는 TestForm.jsx추가

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -2,20 +2,41 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:5000/testResults';
 
+
+
+
 export const getTestResults = async () => {
   const response = await axios.get(API_URL);
   return response.data;
 };
 
+
 export const createTestResult = async (resultData) => {
-  const response = await axios.post(API_URL, resultData);
-  return response.data;
+  try {
+    const response = await axios.post(API_URL, resultData);
+    alert("테스트 결과가 성공적으로 추가되었습니다.");
+    return response.data;
+  } catch (error) {
+    console.error("테스트 결과 추가 중 오류 발생 ==> ", error);
+  }
 };
+
 
 export const deleteTestResult = async (id) => {
-
+  try {
+    const response = await axios.delete(`${API_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("테스트 결과 삭제 중 오류 발생 ==> ", error);
+  }
 };
 
-export const updateTestResultVisibility = async (id, visibility) => {
 
+export const updateTestResultVisibility = async (id, visibility) => {
+  try {
+    const response = await axios.patch(`${API_URL}/${id}`, {visibility});
+    return response.data;
+  } catch (error) {
+    console.error("테스트 결과 업데이트 중 오류 발생 ==> ", error);
+  }
 };
